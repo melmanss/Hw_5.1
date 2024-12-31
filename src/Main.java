e2e8c7b0982e48a50886a53a9c4cd74d4905d5e0
melmanss <74724829+melmanss@users.noreply.github.com>
2024-12-31 08:22:05 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        double income = scanner.nextDouble();
+        double tax = calculateTax(income);
+        System.out.printf("Tax: %.2f", tax);
+    }
+    public static double calculateTax(double income) {
+        double tax = 0;
+        if (income <= 10000) {
+            tax = income * 0.025;
+        } else if (income <= 25000) {
+            tax = 10000 * 0.025 + (income - 10000) * 0.043;
+        } else {
+            tax = 10000 * 0.025 + (25000 - 10000) * 0.043 + (income - 25000) * 0.067;
         }
+        return tax;
     }
 }
